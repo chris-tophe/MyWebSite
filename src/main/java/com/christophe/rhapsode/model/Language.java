36bee06f70a7c5acaafa85bfb44a6c5ab9a49675
Chris-tophe <christophe.loyer@gmail.com>
2020-06-14 23:46:22 +0200

    Ajout language et projet Ajout des tags et du nombre de projets par langage Mise a jour du model

--- src/main/java/com/christophe/rhapsode/model/Language.java
+++ src/main/java/com/christophe/rhapsode/model/Language.java
@@ -1,26 +1,43 @@
 package com.christophe.rhapsode.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Language {
 	
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
     private String name;
     
     private String image;
     
+    @ManyToMany(mappedBy = "languages")
+    private List<Article> articles;
+    
+   
     public Language() {
-    	
+    	articles = new ArrayList<Article>();
     }
     
     public Language(String name) {
-		super();
+    	articles = new ArrayList<Article>();
 		this.name = name;
 	}
     
+	public int getId() {
+		return id;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -35,6 +52,15 @@ public class Language {
 	public void setImage(String image) {
 		this.image = image;
 	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+	
+	public void addArticles(Article article) {
+		articles.add(article);
+	}
+	
 
 	@Override
 	public String toString() {
